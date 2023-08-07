915dc740122aa82e5c32edc3747861d7bc35a9f3
Nahuel Garrido <garridonahuel8@gmail.com>
2023-08-06 23:21:08 -0300

    cors

--- src/main/java/com/techforb/challenge/config/CorsConfig.java
+++ src/main/java/com/techforb/challenge/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://challenge-backend-production.up.railway.app/","https://challenge-backend-production.up.railway.app/", "https://challenge-creditapp.web.app/")
+                .allowedOrigins("http://localhost:4200", "https://challenge-backend-production.up.railway.app/", "https://challenge-creditapp.web.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
